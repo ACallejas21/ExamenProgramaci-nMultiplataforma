3207bb9856c5980639b6ecbcd25065190357831e
Akdiel Callejas <Akdiel.callejas21@gmail.com>
2020-03-13 16:48:28 -0600

    iniciando la creación de la clase estacionamiento.

---
+++ estacionamiento.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+# Programa: Examen II Parcial // clase estacionamiento
+# Autor: Akdiel Callejas Amador
+# Fecha 13/03/20
+
+from vehiculo import Vehiculo
+
+class Estacionamiento:
+    """ representa una lista de onjetos de clase vehículos """
+
+    def __init__(self):
+        """ se encarga de inicializar un listado de objetos tipo vehiculo. """
+        
